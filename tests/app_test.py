8a3e628b3f6ecd694133a24cb15083656ecb949f
sarahagib <sarahagib@gmail.com>
2024-09-30 23:00:38 -0400

    updated app_test with tests for templates and views - they failed as expected.

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,15 +2,82 @@ from pathlib import Path
 
 from project.app import app, init_db
 
+import os 
+import pytest
 
-def test_index():
-    tester = app.test_client()
-    response = tester.get("/", content_type="html/text")
-
-    assert response.status_code == 200
-    assert response.data == b"Hello, World!"
+TEST_DB = 'test.db'
 
 
-def test_database():
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config["TESTING"] = True
+    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+
     init_db()
-    assert Path("flaskr.db").is_file()
+    yield app.test_client()
+    init_db()
+
+
+def login(client, username, password):
+    """Login helper function"""
+    return client.post(
+        "/login",
+        data=dict(username=username, password=password), 
+        follow_redirects=True,
+    )
+
+
+def logout(client):
+    """Logout helper function"""
+    return client.get(
+        "/logout",
+        follow_redirects=True
+    )
+
+
+def test_index(client):
+    response = client.get("/", content_type="html/text")
+    assert response.status_code == 200
+
+
+def test_database(client):
+    """initial test. ensures that the database exists"""
+    tester = Path("test.db").is_file()
+    assert tester
+
+
+def test_empty_db(client):
+    """ensures db is blank"""
+    rv = client.get("/")
+    assert b"No entries yet. Add some!" in rv.data 
+
+
+def test_login_logout(client):
+    """test login/logout functionality using helper function"""
+    
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"]) 
+    assert b"you were logged in" in rv.data
+    rv = logout(client) 
+    assert b"you were logged out" in rv.data
+
+    rv = login(client, app.config["USERNAME"]+"x", app.config["PASSWORD"])
+    assert b"Invalid username" in rv.data
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"]+"x")
+    assert b"Invalid password" in rv.data
+
+
+def test_messages(client):
+    """Ensure that user can post messages"""
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"), 
+        follow_redirects=True,
+    )
+    assert b"No entries here so far" not in rv.data
+    assert b"&lt;Hello&gt;" in rc.data
+    assert b"<strong>HTML</strong> allowed here" in rv.data 
+
+
+
