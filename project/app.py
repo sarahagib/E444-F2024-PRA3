914f6579d6e1a62b1951c76606c8bf892f103898
sarahagib <sarahagib@gmail.com>
2024-10-02 09:52:49 -0400

    got error on render, adding init_db in index.

--- project/app.py
+++ project/app.py
@@ -51,7 +51,11 @@ def close_db(error):
 def index():
     """Searches the database for entries, then displays them."""
     db = get_db()
-    cur = db.execute('select * from entries order by id desc')
+    try:
+        cur = db.execute('select * from entries order by id desc')
+    except:
+        init_db()
+        cur = db.execute('select * from entries order by id desc')
     entries = cur.fetchall()
     return render_template('index.html', entries=entries)
 
