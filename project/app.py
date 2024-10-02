dbfc6fdf594439fc7dabe7bdcbf551947278682c
sarahagib <sarahagib@gmail.com>
2024-10-02 10:42:00 -0400

    made all changes to use sqlalchemy (create_db.py, models.py), changed app_test and app. sqlalchemy works on local run.

--- project/app.py
+++ project/app.py
@@ -1,62 +1,36 @@
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
 import sqlite3
+from pathlib import Path 
+
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy 
+
+
+basedir = Path(__file__).resolve().parent 
 
 # configuration
 DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin" 
 SECRET_KEY = "change_me"
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # create and initialize a new Flask app
 app = Flask(__name__)
-
-
 # load the config
 app.config.from_object(__name__)
+# init sqlalchemy
+db = SQLAlchemy(app) 
 
 
-# connect to database
-def connect_db():
-    """Connects to the database."""
-    rv = sqlite3.connect(app.config["DATABASE"])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-# create the database
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# open database connection
-def get_db():
-    if not hasattr(g, "sqlite_db"):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-# close database connection
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, "sqlite_db"):
-        g.sqlite_db.close()
+from project import models 
 
 
 @app.route("/")
 def index():
     """Searches the database for entries, then displays them."""
-    db = get_db()
-    try:
-        cur = db.execute('select * from entries order by id desc')
-    except:
-        init_db()
-        cur = db.execute('select * from entries order by id desc')
-    entries = cur.fetchall()
+    entries = db.session.query(models.Post)
     return render_template('index.html', entries=entries)
 
 
@@ -89,13 +63,9 @@ def add_entry():
     """add new post to database"""
     if not session.get('logged_in'):
         abort(401) 
-    
-    db = get_db()
-    db.execute(
-        'insert into entries (title, text) values (?, ?)', 
-        [request.form['title'], request.form['text']]
-    )
-    db.commit()
+    new_entry = models.Post(request.form['title'], request.form['text'])
+    db.session.add(new_entry)
+    db.session.commit()
     flash('New entry was successfully posted') 
     return redirect(url_for('index'))
 
@@ -105,9 +75,8 @@ def delete_entry(post_id):
     """Delete post from database"""
     result = {'status': 0, 'message': 'Error'} 
     try:
-        db = get_db()
-        db.execute('delete from entries where id=' + post_id)
-        db.commit()
+        db.session.query(models.Post).filter_by(id=post_id).delete()
+        db.session.commit()
         result = {'status': 1, 'message': 'Post Deleted'}
     except Exception as e: 
         result = {'status': 0, 'message': repr(e)}
